156558fc40f924566fce7c71c507523b03199db8
Ken Chiu <ken.chiu@fullerton.com.tw>
2018-06-23 20:47:03 +0800

    投票程式

---
+++ index.js
@@ -0,0 +1,117 @@
+abi = [
+	{
+		"inputs": [
+			{
+				"name": "candidateNames",
+				"type": "string[]"
+			}
+		],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "constructor"
+	},
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "name",
+				"type": "string"
+			}
+		],
+		"name": "voteForCandidate",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [
+			{
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"name": "candidateList",
+		"outputs": [
+			{
+				"name": "",
+				"type": "string"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [
+			{
+				"name": "name",
+				"type": "string"
+			}
+		],
+		"name": "totalVotesFor",
+		"outputs": [
+			{
+				"name": "",
+				"type": "uint16"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [
+			{
+				"name": "name",
+				"type": "string"
+			}
+		],
+		"name": "validCandidate",
+		"outputs": [
+			{
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	}
+]
+
+VotingContract = web3.eth.contract(abi);
+// In your nodejs console, execute contractInstance.address to get the address at which the contract is deployed and change the line below to use your deployed address
+contractInstance = VotingContract.at('0xf94dea67d7079e04b856662dfbe7a87746a5bb63');
+candidates = {'Ken': 'candidate-1', 'Alduin': 'candidate-2', 'HAHA': 'candidate-3'}
+
+function voteForCandidate() {
+  candidateName = $('#candidate').val();
+  contractInstance.voteForCandidate(candidateName, {from: web3.eth.accounts[0]}, 
+    function() {
+
+    }
+  );
+}
+
+function voteForAll() {
+  candidateNames = ['Ken','Alduin','HAHA'];
+  for(let name of candidateNames){
+    contractInstance.voteForCandidate(name, {from: web3.eth.accounts[0]}, function() {});  
+  }
+}
+
+loop=()=>{
+  let candidateNames = Object.keys(candidates);
+  for (let name of candidateNames) {
+    let val = contractInstance.totalVotesFor.call(name,(e,result)=>{
+      console.log(result);
+      $(`#${candidates[name]}` ).html(result.c[0].toString());
+    })
+  }
+  setTimeout(loop,10000);
+}
+loop()
