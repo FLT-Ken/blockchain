43cbfdae8a8de82180dec44eed223c64e83b68f6
Ken Chiu <ken.chiu@fullerton.com.tw>
2018-06-25 18:22:16 +0800

    第三版合約

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
-abi = [
+abi1 = [
 	{
 		"inputs": [],
 		"payable": false,
@@ -43,28 +43,97 @@ abi = [
 		"type": "function"
 	}
 ]
-VotingContract = web3.eth.contract(abi);
-contractInstance = VotingContract.at('0x5ad26f1bee8d8a273fda64de3937e71ee4f85e9d');
+var contract1 = '0x5ad26f1bee8d8a273fda64de3937e71ee4f85e9d';
 
+abi2 = [
+	{
+		"inputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "constructor"
+	},
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "message",
+				"type": "string"
+			}
+		],
+		"name": "writeMessage",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "getAllMessageByAccount",
+		"outputs": [
+			{
+				"name": "",
+				"type": "string[]"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	}
+]
+var contract2 = '0x4d5370b081653e644103480ca3c8be11e676f2f3';
+
+abi3 = [
+	{
+		"inputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "constructor"
+	},
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "message",
+				"type": "string"
+			}
+		],
+		"name": "writeMessage",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "getAllMessageByAccount",
+		"outputs": [
+			{
+				"name": "",
+				"type": "string"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	}
+]
+var contract3 = '0x795c9fba7bba705388d467c94c43cef79251b4ea';
+
+VotingContract = web3.eth.contract(abi3);
+contractInstance = VotingContract.at(contract3);
 var address = web3.eth.accounts[0];
 
 function sendMessage() {
   let message = $("#message").val();
-  contractInstance.writeMessage(address, message, {from: address}, function() {});
+  contractInstance.writeMessage(message, {from: address}, function() {});
 }
 
 loop=() => {
-  contractInstance.getAllMessageByAccount.call(address, (e, result) => {
+  contractInstance.getAllMessageByAccount.call((e, result) => {
     console.log("result: " + JSON.stringify(result));
-    let messages = '';
-    for(let message of result) {
-      if(messages == '') {
-        messages += message;
-      } else {
-        messages += ", " + message;
-      }
-    }
-    $("#messageArea").html(messages);
+    $("#messageArea").html(result);
   })
   setTimeout(loop, 5000);
 }
